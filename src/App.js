050d0e738afe2e9b28ca074e9712d7808bee95d7
Habidhossen <habidhossen2@gmail.com>
2022-04-04 00:52:00 +0600

    tailwind and react router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Welcome to the react world</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
