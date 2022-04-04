e12be59d0c25734a444b9c8d8e06ca764c4c165e
Habidhossen <habidhossen2@gmail.com>
2022-04-05 05:34:19 +0600

    finish

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,14 +4,13 @@ import image from "../../assets/images/404_img.svg";
 
 const NotFound = () => {
   return (
-    <section className="grid grid-cols-2 container mx-auto">
+    <section className="grid sm:grid-cols-1 md:grid-cols-2 lg:md:grid-cols-2 container mx-auto">
       <div className="my-auto">
-        <h1 className="mb-10 text-4xl font-bold">
-          Oops! The page you're looking for isn't here.
-        </h1>
+        <h1 className="mb-4 text-5xl font-bold">Oops!</h1>
+        <p className="mb-6 text-xl">The page you're looking for isn't here.</p>
         <Link
           to="/"
-          className="w-full py-2 px-5 text-white bg-emerald-400 rounded-full"
+          className="w-full py-2 px-5 text-white bg-emerald-700 rounded-full"
         >
           Back to Home
         </Link>
--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -10,12 +10,10 @@ const ReviewCard = ({ review }) => {
     <div className="custom-card">
       <img className="card-img" src={image} alt="" />
       <p className="card-rating">
-        Rating: {rating} <FontAwesomeIcon icon={faStar} />
+        Rating: <span className="font-semibold">{rating}</span>{" "}
+        <FontAwesomeIcon icon={faStar} />
       </p>
-      <p className="card-text">
-        &#8220; {msg}
-        &#8221;
-      </p>
+      <p className="card-text">&#8220; {msg} &#8221;</p>
       <h1 className="card-title">&#8212; {name}</h1>
     </div>
   );
